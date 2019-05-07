a8d0be804bf49095daee01d8b757d320bb2618f6
igor araujo <igor.araujo@fulllab.com.br>
2019-05-07 16:57:59 -0300

    new api implemented

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,7 +1,10 @@
 class CurrenciesController < ApplicationController
   def index
-    @date = params[:date].blank? ? Date.today : params[:date]
-    @rates = Currencies::Index.call(@date).response
-  # @rates = {"USDEUR"=>0.89208, "USDBRL"=>3.9697, "USDARS"=>44.689007}
+    rates = Currencies::Index.call.response
+
+    @series = []
+    rates['rates'].each do |rate|
+      @series << { name: rate[0], data: rate[1].map{|k,v| "#{v}".to_f} }
+    end
   end
 end
--- app/operations/currencies/index.rb
+++ app/operations/currencies/index.rb
@@ -1,18 +1,19 @@
 class Currencies::Index
-
   attr_accessor :response
 
-  def self.call(date)
-    this = new(date)
+  def self.call
+    this = new
     this.call
     this
   end
 
-  def initialize(date)
-    @date       = date.blank? ? Date.today : date
+  def initialize
     @key        = '4681edf90fa1de308c0571241e4682e3'
-    @url        = 'http://apilayer.net/api/historical'
-    @currencies = 'EUR, BRL, ARS'
+    @url        = 'https://api.exchangeratesapi.io/history'
+    @symbols    = 'MXN,USD,EUR'
+    @base       = 'BRL'
+    @start_at   = 1.week.ago.strftime('%Y-%m-%d')
+    @end_at     = Date.today.strftime('%Y-%m-%d')
   end
 
   def call
@@ -22,7 +23,7 @@ class Currencies::Index
   private
 
   def format_currencies
-    @response = HTTParty.get("#{@url}?currencies=#{@currencies}&access_key=#{@key}&date=#{@date}")['quotes']
+    @response = HTTParty.get("#{@url}?base=#{@base}&start_at=#{@start_at}&end_at=#{@end_at}&symbols=#{@symbols}")
   end
 
 end
