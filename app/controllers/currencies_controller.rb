e0742f3a416e19c370bd19c17031dced76aeab68
igor araujo <igor.araujo@fulllab.com.br>
2019-05-07 01:54:58 -0300

    setup ugly index view

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,5 +1,6 @@
 class CurrenciesController < ApplicationController
   def index
-    @rates = Currencies::Index.call(params[:date]).response
+    # @rates = Currencies::Index.call(params[:date]).response
+  @rates = {"USDEUR"=>0.89208, "USDBRL"=>3.9697, "USDARS"=>44.689007}
   end
 end
