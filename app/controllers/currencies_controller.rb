58c60992186e5753a8f46f5fa21a4485f79266b5
igor araujo <igor.araujo@fulllab.com.br>
2019-05-10 09:27:11 -0300

    currencies controller refactor

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,19 +1,21 @@
 class CurrenciesController < ApplicationController
   def index
-    @filters= {
-        base:   params[:base]   || 'BRL',
-        target: params[:target] || 'USD'
+    @filters = {
+      base:   params[:base]   || 'BRL',
+      target: params[:target] || 'USD'
     }
     rates = Currencies::Index.call(@filters).response
+    set_data(rates['rates'])
+  end
 
+  private
 
-    @series = []
+  def set_data(rates)
+    @series     = []
     @categories = []
-    rates['rates'].each do |rate|
-      @series << rate[1].map{|k,v| "#{v}".to_f }
+    rates.each do |rate|
+      @series     << rate[1].map{|k,v| "#{v}".to_f }
       @categories << rate[0]
     end
-
-
   end
 end
